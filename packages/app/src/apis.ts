693d92f211d91cbfd23519cb07e0c87fb855f362
blam <ben@blam.sh>
2020-11-04 15:45:13 +0100

    feat: added heroku workflow

--- packages/app/src/apis.ts
+++ packages/app/src/apis.ts
@@ -1,1 +1,26 @@
-export const apis = [];
+import {
+  createApiFactory,
+  githubAuthApiRef,
+  discoveryApiRef,
+  oauthRequestApiRef,
+  GithubAuth,
+  configApiRef,
+} from '@backstage/core';
+
+export const apis = [
+  createApiFactory({
+    api: githubAuthApiRef,
+    deps: {
+      configApi: configApiRef,
+      discoveryApi: discoveryApiRef,
+      oauthRequestApi: oauthRequestApiRef,
+    },
+    factory: ({ discoveryApi, oauthRequestApi, configApi }) =>
+      GithubAuth.create({
+        discoveryApi,
+        oauthRequestApi,
+        defaultScopes: ['read:user'],
+        environment: configApi.getString('auth.environment'),
+      }),
+  }),
+];
