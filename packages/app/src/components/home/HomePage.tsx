355f0cdb9464a778843a1e087fb5ed57f708d00f
Yogendra.Singh <yogendra.singh@BA-000135240.local>
2023-09-15 17:54:56 +0530

    Changes for Homepage

--- packages/app/src/components/home/HomePage.tsx
+++ packages/app/src/components/home/HomePage.tsx
@@ -1,6 +1,32 @@
 import React from 'react';
 
-export const HomePage = () => (
-  /* We will shortly compose a pretty homepage here. */
-  <h1>Welcome to Backstage!</h1>
-);
+export const HomePage = () => {
+  // Define some basic styles
+  const containerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',  // Makes the container take up the full viewport height
+    backgroundColor: '#282c34', // Dark background
+    color: 'white', // Text color
+    fontFamily: '"Arial", sans-serif'
+  };
+
+  const headingStyle = {
+    fontSize: '3rem',
+    fontWeight: 'bold',
+    marginBottom: '1rem'
+  };
+
+  const subHeadingStyle = {
+    fontSize: '1.5rem'
+  };
+
+  return (
+    <div style={containerStyle}>
+      <h1 style={headingStyle}>Hello Shiva</h1>
+      <p style={subHeadingStyle}>Welcome to Backstage!</p>
+    </div>
+  );
+};
