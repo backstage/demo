2179d00d296b1502a36362b1b70c2be2f4e634c6
Andre Wanlin <awanlin@spotify.com>
2023-11-09 09:30:29 -0600

    feat: migrated to new backend system again
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>

--- packages/backend/src/plugins/graphql.ts
+++ packages/backend/src/plugins/graphql.ts
@@ -1,3 +1,4 @@
+import { CatalogClient } from '@backstage/catalog-client';
 import { createRouter } from '@frontside/backstage-plugin-graphql-backend';
 import {
   createCatalogLoader,
@@ -8,8 +9,9 @@ import { PluginEnvironment } from '../types';
 
 export default async function createPlugin({
   logger,
-  catalogClient,
+  discovery,
 }: PluginEnvironment): Promise<Router> {
+  const catalogClient = new CatalogClient({ discoveryApi: discovery });
   return await createRouter({
     modules: [Catalog()],
     logger,
