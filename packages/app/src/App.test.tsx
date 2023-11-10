1f56def2d2bdc209219eb443521d94b079b20b8d
Andre Wanlin <awanlin@spotify.com>
2023-11-10 17:51:34 -0600

    chore: fix failing test
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>

--- packages/app/src/App.test.tsx
+++ packages/app/src/App.test.tsx
@@ -3,16 +3,33 @@ import { renderWithEffects } from '@backstage/test-utils';
 import App from './App';
 
 describe('App', () => {
+  
+  const mockRandomUUID = () =>
+    '068f3129-7440-4e0e-8fd4-xxxxxxxxxxxx'.replace(
+      /x/g,
+      () => Math.floor(Math.random() * 16).toString(16), // 0x0 to 0xf
+    ) as `${string}-${string}-${string}-${string}-${string}`;
+  
+    beforeEach(() => {
+    window.crypto.randomUUID = mockRandomUUID;
+  });
+
   it('should render', async () => {
     process.env = {
       NODE_ENV: 'test',
       APP_CONFIG: [
         {
           data: {
-            app: { title: 'Test' },
-            backend: { baseUrl: 'http://localhost:7000' },
+            app: {
+              title: 'Test',
+              support: { url: 'http://localhost:7007/support' },
+            },
+            backend: { baseUrl: 'http://localhost:7007' },
+            lighthouse: {
+              baseUrl: 'http://localhost:3003',
+            },
             techdocs: {
-              storageUrl: 'http://localhost:7000/api/techdocs/static/docs',
+              storageUrl: 'http://localhost:7007/api/techdocs/static/docs',
             },
           },
           context: 'test',
