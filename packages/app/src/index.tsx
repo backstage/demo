56938cc29bc2403454371bf012addc53067d26e6
Andre Wanlin <awanlin@spotify.com>
2024-01-24 07:42:30 -0600

    Manual changes for release
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,7 +1,5 @@
-import { createRoot } from 'react-dom/client';
-import App from './App';
 import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-const container = document.getElementById('root');
-const root = createRoot(container!);
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
