fbe51e6cc0df7bd8777aa60a836d55e47645515d
Andre Wanlin <awanlin@spotify.com>
2025-01-22 17:54:45 -0600

    Manual changes
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>

--- packages/app/src/App.test.tsx
+++ packages/app/src/App.test.tsx
@@ -1,30 +1,16 @@
 import React from 'react';
 import { render, waitFor } from '@testing-library/react';
 import App from './App';
-import { v4 } from 'uuid';
 
 describe('App', () => {
-  const mockRandomUUID = () =>
-    v4() as `${string}-${string}-${string}-${string}-${string}`;
-
-  beforeEach(() => {
-    window.crypto.randomUUID = mockRandomUUID;
-  });
-
   it('should render', async () => {
     process.env = {
       NODE_ENV: 'test',
       APP_CONFIG: [
         {
           data: {
-            app: {
-              title: 'Test',
-              support: { url: 'http://localhost:7007/support' },
-            },
+            app: { title: 'Test' },
             backend: { baseUrl: 'http://localhost:7007' },
-            lighthouse: {
-              baseUrl: 'http://localhost:3003',
-            },
             techdocs: {
               storageUrl: 'http://localhost:7007/api/techdocs/static/docs',
             },
@@ -35,6 +21,7 @@ describe('App', () => {
     };
 
     const rendered = render(<App />);
+
     await waitFor(() => {
       expect(rendered.baseElement).toBeInTheDocument();
     });
