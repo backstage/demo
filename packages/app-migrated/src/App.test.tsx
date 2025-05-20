b5d99d2763d2c9b55d0408c0ddda5f95a1f47457
Andre Wanlin <awanlin@spotify.com>
2025-05-20 12:05:49 -0500

    Removed usage of uuid
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>

--- packages/app-migrated/src/App.test.tsx
+++ packages/app-migrated/src/App.test.tsx
@@ -1,26 +1,18 @@
 import { render, waitFor } from '@testing-library/react';
 import app from './App';
-import { v4 } from 'uuid';
 
 describe('App', () => {
-  const mockRandomUUID = () =>
-    v4() as `${string}-${string}-${string}-${string}-${string}`;
-
-  beforeEach(() => {
-    window.crypto.randomUUID = mockRandomUUID;
-  });
-
   it('should render', async () => {
     process.env = {
       NODE_ENV: 'test',
       APP_CONFIG: [
         {
           data: {
-            app: { title: 'Test' },
-            backend: { baseUrl: 'http://localhost:7007' },
-            techdocs: {
-              storageUrl: 'http://localhost:7007/api/techdocs/static/docs',
+            app: {
+              title: 'Test',
+              support: { url: 'http://localhost:7007/support' },
             },
+            backend: { baseUrl: 'http://localhost:7007' },
           },
           context: 'test',
         },
