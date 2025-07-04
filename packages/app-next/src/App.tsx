0ced91cd8f582a435dc37b9c9d856516bea651ec
Andre Wanlin <awanlin@spotify.com>
2025-07-04 11:27:05 -0500

    Initial migration
    Signed-off-by: Andre Wanlin <awanlin@spotify.com>

--- packages/app-next/src/App.tsx
+++ packages/app-next/src/App.tsx
@@ -1,20 +1,10 @@
 import { badgesPlugin } from './plugins';
 
-import {
-  AlertDisplay,
-  OAuthRequestDialog,
-  ProxiedSignInPage,
-} from '@backstage/core-components';
-import { AppRouter, FlatRoutes } from '@backstage/core-app-api';
-import {
-  CatalogEntityPage,
-  CatalogIndexPage,
-  catalogPlugin,
-} from '@backstage/plugin-catalog';
-import {
-  CatalogGraphPage,
-  catalogGraphPlugin,
-} from '@backstage/plugin-catalog-graph';
+import { ProxiedSignInPage } from '@backstage/core-components';
+import { FlatRoutes } from '@backstage/core-app-api';
+import { CatalogIndexPage, catalogPlugin } from '@backstage/plugin-catalog';
+import { default as alphaCatalogPlugin } from '@backstage/plugin-catalog/alpha';
+import { catalogGraphPlugin } from '@backstage/plugin-catalog-graph';
 import {
   CostInsightsLabelDataflowInstructionsPage,
   CostInsightsPage,
@@ -29,12 +19,8 @@ import {
 } from '@backstage/plugin-techdocs';
 import { UnifiedThemeProvider, themes } from '@backstage/theme';
 
-import { ApiExplorerPage } from '@backstage/plugin-api-docs';
 import { GraphiQLPage } from '@backstage-community/plugin-graphiql';
 
-import { Root } from './components/Root';
-import { SearchPage } from '@backstage/plugin-search';
-import { TechRadarPage } from '@backstage-community/plugin-tech-radar';
 import {
   SettingsLayout,
   UserSettingsPage,
@@ -42,14 +28,12 @@ import {
 import { apertureTheme } from './theme/aperture';
 import { apis } from './apis';
 
-import { entityPage } from './components/catalog/EntityPage';
 import { orgPlugin } from '@backstage/plugin-org';
-import { searchPage } from './components/search/SearchPage';
+
 import { CssBaseline } from '@material-ui/core';
 import { HomepageCompositionRoot, VisitListener } from '@backstage/plugin-home';
 import { CustomizableHomePage } from './components/home/CustomizableHomePage';
-import { ScaffolderPage, scaffolderPlugin } from '@backstage/plugin-scaffolder';
-import { NotificationsPage } from '@backstage/plugin-notifications';
+import { scaffolderPlugin } from '@backstage/plugin-scaffolder';
 import { TechDocsAddons } from '@backstage/plugin-techdocs-react';
 import {
   ExpandableNavigation,
@@ -64,49 +48,35 @@ import { NotificationSettings } from './components/settings/NotificationSettings
 // New Frontend System Imports
 import { createApp } from '@backstage/frontend-defaults';
 import {
+  compatWrapper,
   convertLegacyApp,
   convertLegacyAppOptions,
   convertLegacyRouteRef,
   convertLegacyRouteRefs,
 } from '@backstage/core-compat-api';
 import {
+  AppRootElementBlueprint,
   createFrontendModule,
   SignInPageBlueprint,
   ThemeBlueprint,
 } from '@backstage/frontend-plugin-api';
+import { rootNav } from './components/Root';
+import {
+  EntityKindPicker,
+  EntityTypePicker,
+  UserListPicker,
+  EntityOwnerPicker,
+  EntityLifecyclePicker,
+  EntityTagPicker,
+  EntityProcessingStatusPicker,
+  EntityNamespacePicker,
+} from '@backstage/plugin-catalog-react';
 
 const routes = (
   <FlatRoutes>
     <Route path="/" element={<Navigate to="catalog" />} />
     <Route path="/home" element={<HomepageCompositionRoot />}>
       <CustomizableHomePage />
-    </Route>
-    {/* <FeatureFlagged with="customizable-home-page-preview">
-      <Route path="/home" element={<HomepageCompositionRoot />}>
-        <CustomizableHomePage />
-      </Route>
-    </FeatureFlagged>
-    <FeatureFlagged without="customizable-home-page-preview">
-      <Route path="/home" element={<HomepageCompositionRoot />}>
-        <HomePage />
-      </Route>
-    </FeatureFlagged> */}
-    <Route path="/create" element={<ScaffolderPage />} />
-    <Route path="/api-docs" element={<ApiExplorerPage />} />
-    <Route
-      path="/catalog"
-      element={
-        <CatalogIndexPage
-          initiallySelectedFilter="all"
-          initiallySelectedNamespaces={['default']}
-        />
-      }
-    />
-    <Route
-      path="/catalog/:namespace/:kind/:name"
-      element={<CatalogEntityPage />}
-    >
-      {entityPage}
     </Route>
     <Route path="/cost-insights" element={<CostInsightsPage />} />
     <Route
@@ -133,31 +103,30 @@ const routes = (
 
     <Route path="/explore" element={<ExplorePage />} />
     <Route path="/graphiql" element={<GraphiQLPage />} />
-    <Route path="/search" element={<SearchPage />}>
-      {searchPage}
-    </Route>
+
     <Route path="/settings" element={<UserSettingsPage />}>
       <SettingsLayout.Route path="/notifications" title="Notifications">
         <NotificationSettings />
       </SettingsLayout.Route>
     </Route>
-    <Route path="/tech-radar" element={<TechRadarPage />} />
-    <Route path="/catalog-graph" element={<CatalogGraphPage />} />
-    <Route path="/notifications" element={<NotificationsPage />} />
   </FlatRoutes>
 );
 
-const legacyFeatures = convertLegacyApp(
-  <>
-    <AlertDisplay />
-    <OAuthRequestDialog />
-    <SignalsDisplay />
-    <AppRouter>
-      <VisitListener />
-      <Root>{routes}</Root>
-    </AppRouter>
-  </>,
-);
+const legacyFeatures = convertLegacyApp(routes);
+
+const signalsDisplayExtension = AppRootElementBlueprint.make({
+  name: 'signals-display-extension',
+  params: {
+    element: compatWrapper(<SignalsDisplay />),
+  },
+});
+
+const visitListenerExtension = AppRootElementBlueprint.make({
+  name: 'visit-listener-extension',
+  params: {
+    element: <VisitListener />,
+  },
+});
 
 const optionsModule = convertLegacyAppOptions({
   // TODO:(awanlin) the badges plugin doesn't support the new frontend system yet
@@ -217,6 +186,34 @@ const apertureThemeExtension = ThemeBlueprint.make({
   },
 });
 
+const catalogPluginOverride = alphaCatalogPlugin.withOverrides({
+  extensions: [
+    alphaCatalogPlugin.getExtension('page:catalog').override({
+      params: {
+        loader: async () =>
+          compatWrapper(
+            <CatalogIndexPage
+              filters={
+                <>
+                  <EntityKindPicker />
+                  <EntityTypePicker />
+                  <UserListPicker initialFilter="all" />
+                  <EntityOwnerPicker />
+                  <EntityLifecyclePicker
+                    initialFilter={['production', 'experimental']}
+                  />
+                  <EntityTagPicker />
+                  <EntityProcessingStatusPicker />
+                  <EntityNamespacePicker />
+                </>
+              }
+            />,
+          ),
+      },
+    }),
+  ],
+});
+
 const app = createApp({
   features: [
     optionsModule,
@@ -229,8 +226,12 @@ const app = createApp({
         lightThemeExtension,
         darkThemeExtension,
         apertureThemeExtension,
+        rootNav,
+        signalsDisplayExtension,
+        visitListenerExtension,
       ],
     }),
+    catalogPluginOverride,
   ],
   bindRoutes({ bind }) {
     bind(convertLegacyRouteRefs(catalogPlugin.externalRoutes), {
